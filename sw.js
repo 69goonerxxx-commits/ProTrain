1146233c3670ba4eb58ae60f4a41851915aa54df
69goonerxxx-commits <69gooner.xxx@gmail.com>
2026-05-30 20:14:46 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'protrain-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (new URL(e.request.url).origin === self.location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(c => c || fetch(e.request).then(r => {
+        caches.open(CACHE).then(cache => cache.put(e.request, r.clone()));
+        return r;
+      }))
+    );
+  }
+});
