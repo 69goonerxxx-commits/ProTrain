d88931c2332e6eff4139daa95ce93498293a8547
69goonerxxx-commits <69gooner.xxx@gmail.com>
2026-06-01 20:07:05 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,37 @@
 const CACHE = 'protrain-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;800&family=Barlow:wght@400;500;600&display=swap'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', e => {
-  if (new URL(e.request.url).origin === self.location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(c => c || fetch(e.request).then(r => {
-        caches.open(CACHE).then(cache => cache.put(e.request, r.clone()));
-        return r;
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
       }))
-    );
-  }
+      .catch(() => caches.match('./index.html'))
+  );
 });
